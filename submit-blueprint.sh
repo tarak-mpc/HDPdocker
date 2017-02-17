6f580dc937dde47e44166cfe4a0ce56fa541ea90
tarak-mpc <tarak.mpc@gmail.com>
2017-02-17 14:51:48 +0100

    Update submit-blueprint.sh

--- submit-blueprint.sh
+++ submit-blueprint.sh
@@ -1,7 +1,8 @@
 TYPE=$1
 BLUEPRINT=$2
+CLUSTER="HDPdocker"
 source ./.env
 
 curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/blueprints/$TYPE --data-binary "@$BLUEPRINT"
 curl --user admin:admin -H 'X-Requested-By:admin' -X PUT $AMBARI_HOST/api/v1/stacks/HDP/versions/$HDP_VERSION/operating_systems/$OS/repositories/HDP-${HDP_VERSION} -d '{"Repositories":{"base_url":"'$BASE_URL'", "verify_base_url":true}}'
-curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/clusters/dev --data-binary "@examples/hostgroups/$TYPE.json"
+curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/clusters/$CLUSTER --data-binary "@examples/hostgroups/$TYPE.json"
